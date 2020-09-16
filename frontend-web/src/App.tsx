bdf212f3d89ef69e0faf9dc0e2a1a79d84dc01b4
douglas <douglasbraga_lp@hotmail.com>
2020-09-16 12:44:06 -0300

    adicionando component Header

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-	return <div></div>;
+	return <Header></Header>;
 }
 
 export default App;
